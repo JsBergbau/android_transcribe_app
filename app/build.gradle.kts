d9e0c9c8dab6f68b79d30c3f25c1b6fe17aafb2e
Noah Mühl <noahmuehl@me.com>
2026-02-18 00:04:36 +0100

    v0.1.12: - Expose Offline Voice Input to external keyboards (e.g. SwiftKey) (thanks @JsBergbau) - Add animation for voice input (RecognizeActivity) thanks @JsBergbau - Improve build process (moved to Gradle)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.notune.transcribe"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "0.1.11"
+        versionCode = 13
+        versionName = "0.1.12"
         ndk {
             abiFilters += "arm64-v8a"
         }
